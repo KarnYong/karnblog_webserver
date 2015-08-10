c31c968c557604f8ec8c562ef87b99ac04dae229
Karn Yongsiriwit <karnyong@gmail.com>
2015-08-10 15:14:48 +0200

    Added nova theme

--- karnblog/settings.py
+++ karnblog/settings.py
@@ -204,6 +204,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "nova",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
